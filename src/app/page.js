ffd14102e7ec4e5d22fc5fe37a14fb0bbb7c0d60
touseeqijaz <touseeqijaz@gmail.com>
2025-03-04 19:12:53 +0500

    Add flow of create order

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import ChatInterface from "./components/ChatInterface";
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#000000] p-8">
